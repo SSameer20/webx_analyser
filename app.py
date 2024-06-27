cadc415067b5e0c62a75f83d02bf07a81f20bc9d
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-06-27 21:44:00 +0530

    app update

--- app.py
+++ app.py
@@ -7,27 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/analyze', methods=['GET', 'POST'])
+@app.route('/analyze')
 def analyze():
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            url = data['url']
-            url_info = extract_url_info(url)
-            return jsonify({"message": "URL processed successfully!", "url_info": url_info})
-        else:
-            return jsonify({"error": "Unsupported Media Type"}), 415
     return render_template('analyze.html')
-
-def extract_url_info(url):
-    extracted = tldextract.extract(url)
-    url_info = {
-        "subdomain": extracted.subdomain,
-        "domain": extracted.domain,
-        "suffix": extracted.suffix,
-        "registered_domain": extracted.registered_domain
-    }
-    return url_info
 
 @app.route('/about')
 def about():
